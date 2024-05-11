a7bf93251f93f1a9b7200d3eb6f909a5e2ad8588
Hovtoi <hovtoi@gmail.com>
2024-05-11 11:00:01 +0700

    toi cmit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.menu-2').addEventListener('click', function() {
+    document.getElementById('wordFrame').setAttribute('src', 'https://drive.google.com/file/d/1YYYITSgMv1KFtCnrQc_jHyq-Nkd4vIYp/preview');
+    var thiss = document.querySelector('.menu-2');
+    thiss.style.color = 'white';
+    thiss.style.backgroundColor = 'lightblue';
+});
